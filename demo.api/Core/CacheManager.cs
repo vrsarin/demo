f027e0adda7142f3a9ef278f267141eb5d962b6d
Vivek Sarin <vrsarin@hotmail.com>
2022-09-18 00:20:08 -0400

    draft

---
+++ demo.api/Core/CacheManager.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Threading.Tasks;
+using System.Threading;
+using System;
+
+namespace demo.api.Core
+{
+    public class CacheManager : ICacheManager
+    {
+        private readonly IDistributedCache cache;
+
+        public CacheManager(IDistributedCache cache)
+        {
+            this.cache = cache;
+        }
+
+        public async Task<byte[]> GetAsync(
+            string key,
+            DistributedCacheEntryOptions cacheOptions = default(DistributedCacheEntryOptions),
+            CancellationToken token = default(CancellationToken))
+        {
+
+            var cacheValue = await this.cache.GetAsync(key, token);
+            if (cacheValue is object)
+            {
+                return cacheValue;
+            }
+
+            return default;
+        }
+
+
+    }
+}
---
+++ demo.api/Core/ICacheManager.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace demo.api.Core
+{
+    public interface ICacheManager
+    {
+        Task<byte[]> GetAsync(string key, DistributedCacheEntryOptions cacheOptions = null, CancellationToken token = default);
+    }
+}
