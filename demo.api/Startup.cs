2201cb2c212f8d0414a59f19b7275aa85af55fc9
Vivek Sarin <vrsarin@hotmail.com>
2022-09-24 22:55:24 -0400

    Adding basic logging and helm chart

--- demo.api/Startup.cs
+++ demo.api/Startup.cs
@@ -1,45 +1,57 @@
 using demo.api.Setup;
 using Microsoft.AspNet.OData.Extensions;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace demo.api
 {
     public class Startup
     {
+   
 
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+           ;
+  
         }
 
         public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
+            Log.Logger.Information("Started Startup ConfigureServices");
             services.AddControllers();
+            services.AddHealthChecks();
+           
             //services.AddSingleton<IApplicationSettings, ApplicationSettings>();
             //services.AddOData();
-            services.AddStackExchangeRedisCache(options =>
-            {
-                options.Configuration = Configuration.GetValue<string>("CacheConnection");
-            });
+            //services.AddStackExchangeRedisCache(options =>
+            //{
+            //    options.Configuration = Configuration.GetValue<string>("CacheConnection");
+            //});
             services.SetupApiVersion();
             services.SetupSwaggerDocumentation();
+            Log.Logger.Information("Completed Startup ConfigureServices");
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider provider)
         {
+            Log.Logger.Information("Started Startup Configure");
+            Log.Logger.Information(Configuration["apiConfig:basePath"]);
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
+       
+            app.UseSerilogRequestLogging();
             //app.UseHttpsRedirection();
             app.UsePathBase(Configuration["apiConfig:basePath"]);
 
@@ -52,7 +64,9 @@ namespace demo.api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHealthChecks(string.Empty);
             });
+            Log.Logger.Information("Completed Startup Configure");
         }
     }
 }
